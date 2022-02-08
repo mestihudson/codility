96438e2970d1dd9efd9435146a23177560d3f739
mestihudson <mestihudson@gmail.com>
2022-02-08 15:58:45 -0300

    test: should return lower difference between splits

---
+++ tape-equilibrium.spec.js
@@ -0,0 +1,8 @@
+const { solution } = require("./tape-equilibrium")
+
+it("should return lower difference between splits", () => {
+  expect(solution([1, 2])).toBe(1)
+  expect(solution([1, 2, 3])).toBe(0)
+  expect(solution([3, 1, 2, 4, 3])).toBe(1)
+  expect(solution([1, 2, 3, 4])).toBe(2)
+})
