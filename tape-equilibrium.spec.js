c044e53c3b8c7595093b53ccc9cf08b611ca53d3
mestihudson <mestihudson@gmail.com>
2022-02-08 21:32:19 -0300

    test: should return lower difference between splits (add border case)

--- tape-equilibrium.spec.js
+++ tape-equilibrium.spec.js
@@ -5,6 +5,7 @@ it("should return lower difference between splits", () => {
   expect(solution([1, 2, 3])).toBe(0)
   expect(solution([3, 1, 2, 4, 3])).toBe(1)
   expect(solution([1, 2, 3, 4])).toBe(2)
+  expect(solution([-1000, 1000])).toBe(2000)
   try {
     solution([-1001, 1001])
     expect(true).toFalsy()
