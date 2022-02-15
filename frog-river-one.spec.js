a077fec0895de0734866ddeb5dd0344a13cbc2d9
mestihudson <mestihudson@gmail.com>
2022-02-14 23:11:56 -0300

    test: change assertion of third case

--- frog-river-one.spec.js
+++ frog-river-one.spec.js
@@ -3,6 +3,6 @@ const { solution } = require("./frog-river-one")
 it("should return frog river one", () => {
   expect(solution(5, [1, 3, 1, 4, 2, 3, 5, 4])).toBe(6)
   expect(solution(2, [2, 2, 2, 2, 2])).toBe(-1)
-  expect(solution(2, [2, 2, 2, 2, 1])).toBe(4)
+  expect(solution(2, [2, 2, 2, 3, 1, 1])).toBe(4)
   expect(solution(7, [7, 8, 2, 3, 8, 3, 2, 1])).toBe(-1)
 })
