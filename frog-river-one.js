26257d666a3ffe8865cac594922e14bf67979d84
mestihudson <mestihudson@gmail.com>
2022-02-14 23:12:16 -0300

    feat: another solution
      https://app.codility.com/demo/results/trainingVVQ8NT-DPP/
      https://www.alessiocantarella.it/codility/frog-river-one/

--- frog-river-one.js
+++ frog-river-one.js
@@ -1,12 +1,24 @@
-function solution(moment, leaves) {
-  let setOfLeaves = new Set()
-  for (let i = 0; i < leaves.length; i++) {
-    setOfLeaves.add(leaves[i])
-    if (setOfLeaves.size === moment) {
-      return i
+function solution(x, A) {
+  let B = [];
+  for (let i = 0; i < x; i++) {
+    B[i] = -1;
+  }
+  let n = A.length;
+  for (let i = 0; i < n; i++) {
+    if (B[A[i] - 1] == -1) {
+      B[A[i] - 1] = i;
     }
   }
-  return -1
+  let r = 0;
+  for (let i in B) {
+    let b = B[i];
+    if (b > r) {
+      r = b;
+    } else if (b == -1) {
+      return -1;
+    }
+  }
+  return r;
 }
 
 module.exports = { solution }
